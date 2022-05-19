b876e177c6bf4b796447c86430d2878458312270
Ankush Sharma <ankushpndt@gmail.com>
2022-05-19 19:06:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Canvas from "./Canvas";
+import React, { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const initialShape = [
+		{
+			x: 10,
+			y: 10,
+			width: 100,
+			height: 100,
+
+			id: "image1",
+		},
+	];
+
+	const [show, setShow] = useState(false);
+	return (
+		<div className="App">
+			<h1>Canvas</h1>
+			<button onClick={() => setShow(!show)}>Show text </button>
+
+			<Canvas shapeProps={initialShape} show={show} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Canvas.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Stage, Layer, Image, Text } from "react-konva";
+const Canvas = ({ show }) => {
+	const [image, setImage] = useState(new window.Image());
+
+	useEffect(() => {
+		const img = new window.Image();
+		img.src =
+			"https://images.unsplash.com/photo-1531804055935-76f44d7c3621?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80";
+		setImage(img);
+	}, []);
+
+	//video
+
+	const usePreview = (url) => {
+		const [canvas, setCanvas] = useState(null);
+
+		useEffect(() => {
+			const video = document.createElement("video");
+			video.src = url;
+			const onLoad = () => {
+				const canvas = document.createElement("canvas");
+				canvas.width = video.videoWidth;
+				canvas.height = video.videoHeight;
+				video.currentTime = 1;
+				const ctx = canvas.getContext("2d");
+				ctx.drawImage(video, 0, 0);
+				setCanvas(canvas);
+			};
+			video.addEventListener("canplay", onLoad);
+			return () => video.removeEventListener("load", onLoad);
+		}, []);
+
+		return canvas;
+	};
+
+	const video = useRef();
+	const imageRef = useRef();
+	const anim = useRef();
+
+	const [isPlaying, setPlaying] = useState(false);
+
+	const preview = usePreview(
+		"https://upload.wikimedia.org/wikipedia/commons/transcoded/c/c4/Physicsworks.ogv/Physicsworks.ogv.240p.vp9.webm"
+	);
+	useEffect(() => {
+		anim.current = new window.Konva.Animation(() => {
+			image?.current?.image(video?.current);
+		}, image?.current?.getLayer());
+	}, [image]);
+	useEffect(() => {
+		if (isPlaying) {
+			video?.current?.play();
+			anim?.current?.start();
+		} else {
+			video?.current?.pause();
+			anim?.current?.stop();
+		}
+	}, [isPlaying]);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					setPlaying(true);
+					video?.current?.play();
+					anim?.current?.start();
+				}}
+			>
+				Play
+			</button>
+			<button
+				onClick={() => {
+					video?.current?.pause();
+					anim?.current?.stop();
+				}}
+			>
+				Pause
+			</button>
+			<button
+				onClick={() => {
+					video.current.stop();
+					anim.current.stop();
+				}}
+			>
+				Stop
+			</button>
+			<video
+				style={{ display: "none" }}
+				ref={video}
+				src="https://upload.wikimedia.org/wikipedia/commons/transcoded/c/c4/Physicsworks.ogv/Physicsworks.ogv.240p.vp9.webm"
+			/>
+			<Stage width={window.innerWidth} height={window.innerHeight}>
+				<Layer>
+					{show && <Text text="HI" />}
+
+					<Image x={100} y={200} image={image} />
+
+					<Image
+						x={100}
+						y={100}
+						ref={imageRef}
+						width={200}
+						height={100}
+						stroke="black"
+						image={isPlaying ? video.current : preview}
+					/>
+				</Layer>
+			</Stage>
+		</div>
+	);
+};
+
+export default Canvas;
